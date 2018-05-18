8160158d7854de5b534112ea9c3f165605bea226
Matt Calabrese <matt@boost.org>
2018-05-17 20:53:29 -0400

    Rename, fix, and test prov::element_or_nothing (formerly prov::element_if_has_value).

--- include/argot/prov/element_or_nothing.hpp
+++ include/argot/prov/element_or_nothing.hpp
@@ -5,132 +5,149 @@
   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 
-#ifndef ARGOT_PROV_ELEMENT_IF_HAS_VALUE_HPP_
-#define ARGOT_PROV_ELEMENT_IF_HAS_VALUE_HPP_
+#ifndef ARGOT_PROV_ELEMENT_OR_NOTHING_HPP_
+#define ARGOT_PROV_ELEMENT_OR_NOTHING_HPP_
 
 #include <argot/basic_result_of.hpp>
 #include <argot/concepts/argument_provider.hpp>
 #include <argot/concepts/argument_receiver_of.hpp>
+#include <argot/concepts/optional_like.hpp>
 #include <argot/concepts/persistent_argument_provider.hpp>
+#include <argot/concepts/reference.hpp>
+#include <argot/concepts/volatile_object.hpp>
 #include <argot/declval.hpp>
 #include <argot/forward.hpp>
+#include <argot/gen/concept_assert.hpp>
 #include <argot/gen/make_concept_map.hpp>
+#include <argot/gen/not.hpp>
 #include <argot/gen/requires.hpp>
 #include <argot/move.hpp>
+#include <argot/opt_traits/get.hpp>
+#include <argot/opt_traits/has_value.hpp>
 #include <argot/receiver_traits/argument_list_kinds.hpp>
 #include <argot/receiver_traits/argument_types.hpp>
 #include <argot/receiver_traits/receive_branch.hpp>
 #include <argot/remove_cvref.hpp>
 
+#include <memory>
+#include <type_traits>
+
 namespace argot {
 namespace prov {
 
-struct element_if_has_value_fn
+struct element_or_nothing_fn
 {
   template< class Opt >
   struct impl
   {
-    // TODO(mattcalabrese) Concept check
+    ARGOT_CONCEPT_ASSERT( OptionalLike< remove_cvref_t< Opt > > );
+    ARGOT_CONCEPT_ASSERT( Not< VolatileObject< remove_cvref_t< Opt > > > );
+    ARGOT_CONCEPT_ASSERT( Reference< Opt > );
 
     std::remove_reference_t< Opt >* opt;
   };
 
-  // TODO(mattcalabrese) Concept check
-  template< class Opt >
+  template< class Opt
+          , ARGOT_REQUIRES( OptionalLike< remove_cvref_t< Opt > > )
+                          ( Not< VolatileObject< remove_cvref_t< Opt > > > )
+                          ()
+          >
   [[nodiscard]] constexpr auto operator ()( Opt&& opt ) const
   {
-    return impl< Opt&& >{ ARGOT_FORWARD( Opt )( opt ) };
+    return impl< Opt&& >{ std::addressof( opt ) };
   }
-} inline constexpr element_if_has_value{};
+} inline constexpr element_or_nothing{};
 
 template< class Opt >
-using result_of_element_if_has_value
-  = basic_result_of< element_if_has_value_fn const&, Opt >;
+using result_of_element_or_nothing
+  = basic_result_of< element_or_nothing_fn const&, Opt >;
 
 template< class Opt >
-using result_of_element_if_has_value_t
-  = basic_result_of_t< element_if_has_value_fn const&, Opt >;
+using result_of_element_or_nothing_t
+  = basic_result_of_t< element_or_nothing_fn const&, Opt >;
 
 }  // namespace argot(::prov)
 
-// TODO(mattcalabrese) Use traits
 template< class Opt >
 struct make_concept_map
-< ArgumentProvider< prov::element_if_has_value_fn::impl< Opt > > >
+< ArgumentProvider< prov::element_or_nothing_fn::impl< Opt > > >
 {
   template
   < class Receiver
   , ARGOT_REQUIRES
     ( ArgumentReceiverOf
       < Receiver
+      , receiver_traits::argument_types_t
+        < opt_traits::result_of_get_t< Opt&& > >
       , receiver_traits::argument_types_t<>
-      , receiver_traits::argument_types_t< decltype( *ARGOT_DECLVAL( Opt& ) ) >  // TODO(mattcalabrese) Use traits
       >
-    )()
+    )
+    ()
   >
   static constexpr decltype( auto )
-  provide( prov::element_if_has_value_fn::impl< Opt > self
+  provide( prov::element_or_nothing_fn::impl< Opt > self
          , Receiver&& receiver
          )
   {
-    return   self.has_value()  // TODO(mattcalabrese) Use traits
+    return   opt_traits::has_value( *self.opt )
            ? receiver_traits::receive_branch
              ( ARGOT_MOVE( receiver )
+             , receiver_traits::argument_list_kinds()
              , receiver_traits::argument_list_kinds_t
                < receiver_traits::argument_types_t<> >{}
-             , receiver_traits::argument_list_kinds()
-             , **self.opt  // TODO(mattcalabrese) Use traits
+             , opt_traits::get( ARGOT_FORWARD( Opt )( *self.opt ) )
              )
            : receiver_traits::receive_branch
              ( ARGOT_MOVE( receiver )
-             , receiver_traits::argument_list_kinds()
              , receiver_traits::argument_list_kinds_t
                < receiver_traits::argument_types_t
-                 < decltype( *ARGOT_DECLVAL( Opt& ) ) >  // TODO(mattcalabrese) Use traits
+                 < opt_traits::result_of_get_t< Opt&& > >
                >{}
+             , receiver_traits::argument_list_kinds()
              );
   }
 };
 
-// TODO(mattcalabrese) Use traits
 template< class Opt >
 struct make_concept_map
-< PersistentArgumentProvider< prov::element_if_has_value_fn::impl< Opt > > >
+< PersistentArgumentProvider< prov::element_or_nothing_fn::impl< Opt > > >
 {
   template
   < class Receiver
   , ARGOT_REQUIRES
     ( ArgumentReceiverOf
       < Receiver
+      , receiver_traits::argument_types_t
+        < opt_traits::result_of_get_t< Opt const& > >
       , receiver_traits::argument_types_t<>
-      , receiver_traits::argument_types_t< decltype( *ARGOT_DECLVAL( Opt ) ) >  // TODO(mattcalabrese) Use traits
       >
-    )()
+    )
+    ()
   >
   static constexpr decltype( auto )
-  provide( prov::element_if_has_value_fn::impl< Opt > self
+  provide( prov::element_or_nothing_fn::impl< Opt > self
          , Receiver&& receiver
          )
   {
-    return   self.has_value()  // TODO(mattcalabrese) Use traits
+    return   opt_traits::has_value( *self.opt )
            ? receiver_traits::receive_branch
              ( ARGOT_MOVE( receiver )
+             , receiver_traits::argument_list_kinds()
              , receiver_traits::argument_list_kinds_t
                < receiver_traits::argument_types_t<> >{}
-             , receiver_traits::argument_list_kinds()
-             , *ARGOT_FORWARD( Opt )( *self.opt )  // TODO(mattcalabrese) Use traits
+             , opt_traits::get( *self.opt )
              )
            : receiver_traits::receive_branch
              ( ARGOT_MOVE( receiver )
-             , receiver_traits::argument_list_kinds()
              , receiver_traits::argument_list_kinds_t
                < receiver_traits::argument_types_t
-                 < decltype( *ARGOT_DECLVAL( Opt ) ) >  // TODO(mattcalabrese) Use traits
+                 < opt_traits::result_of_get_t< Opt const& > >
                >{}
+             , receiver_traits::argument_list_kinds()
              );
   }
 };
 
 }  // namespace argot
 
-#endif  // ARGOT_PROV_ELEMENT_IF_HAS_VALUE_HPP_
+#endif  // ARGOT_PROV_ELEMENT_OR_NOTHING_HPP_
